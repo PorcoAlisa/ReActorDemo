05899d219eb5438f305e4db9e37eafb85acb3208
store <porco@localhost.localdomain>
2024-10-16 15:18:43 -0400

    为Channel新增处理事件的函数

--- Channel.cpp
+++ Channel.cpp
@@ -34,3 +34,38 @@ void Channel::SetHappenedEvents(uint32_t happenedEvents)
 {
     happenedEvents_ = happenedEvents;
 }
+
+/* 函数名叫handelevents，那handle什么events呢，handle epollwait的时候，监测到发生
+并通过SetHappenedEvents写入happenedEvents_的事件 */
+void Channel::HandEvents()
+{
+    if (happenedEvents_ & EPOLLRDHUP) { /* 说明客户端已关闭连接 */
+        closeCallBack_();
+    } else if (happenedEvents_ & (EPOLLIN|EPOLLPRI)) { /* 接收缓冲区中有数据可以读 */
+        readCallBack_();
+    } else if (happenedEvents_ & EPOLLOUT) { /* 有数据需要写 */
+        writeCallBack_();
+    } else {
+        errorCallBack_();
+    }
+}
+
+void Channel::SetCloseCallBack(function<void()> fn)
+{
+    closeCallBack_ = fn;
+}
+
+void Channel::SetReadCallBack(function<void()> fn)
+{
+    readCallBack_ = fn;
+}
+
+void Channel::SetWriteCallBack(function<void()> fn)
+{
+    writeCallBack_ = fn;
+}
+
+void Channel::SetErrorCallBack(function<void()> fn)
+{
+    errorCallBack_ = fn;
+}
--- Channel.h
+++ Channel.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <stdint.h>
+#include <functional>
+#include <sys/epoll.h>
+using namespace std;
 
 class Channel
 {
@@ -8,6 +11,10 @@ private:
     uint32_t events_ = 0; /* 在添加到epoll模型监视的时候，需要知道监视什么events */
     uint32_t happenedEvents_ = 0; /* epoll模型监测到事件发生后，记录发生了什么事件 */
     bool inEpoll_ = false; /* 添加到epoll模型之后，可能会对监视的事件修改，此时需要区分channel是否已被添加到Epoll（add或mod） */
+    function<void()> closeCallBack_;
+    function<void()> readCallBack_;
+    function<void()> writeCallBack_;
+    function<void()> errorCallBack_;
 public:
     Channel(int fd);
     ~Channel();
@@ -15,5 +22,10 @@ public:
     uint32_t Events();
     int Fd();
     void SetInEpoll(bool inEpoll);
+    void HandEvents();
     void SetHappenedEvents(uint32_t happenedEvents);
+    void SetCloseCallBack(function<void()> fn);
+    void SetReadCallBack(function<void()> fn);
+    void SetWriteCallBack(function<void()> fn);
+    void SetErrorCallBack(function<void()> fn);
 };
